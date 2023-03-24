a6a8cc0be6064555e294b7c77b379863951ffde0
Simon Kraus <kraus_simon@t-online.de>
2023-03-24 21:08:55 +0100

    📝 update privacy policy

--- src/pages/static/privacy.page.js
+++ src/pages/static/privacy.page.js
@@ -16,64 +16,72 @@ const PrivacyPage = () => {
 						<Content>
 							<hr />
 							<Paragraph>
-								We are very delighted that you have shown interest in our enterprise as well as our product, Tiascan. Data protection is of a particularly high priority for the management of the Staking Facilities GmbH.
+								This Privacy Policy explains the processing of your personal data by Staking Facilities Services GmbH & Co. KG, Rosenheimer Str. 18, 85653 Aying, Germany ("we") as the controller (Art. 4 no. 7 GDPR) and your rights under the GDPR.
+							</Paragraph>
+							<h2><strong>1. Purposes of processing</strong></h2>
+							<h3><strong>1.1 Delivery of the website</strong></h3>
+							<Paragraph>
+								If you access our website some personal data is temporarily stored, esp. IP address and browser meta data (e.g., date and time of access and your browser type).
 							</Paragraph>
 							<Paragraph>
-								The use of the Internet pages of the Staking Facilities GmbH is possible without any indication of personal data; however, if a data subject wants to use special enterprise services via our website, processing of personal data could become necessary. If the processing of personal data is necessary and there is no statutory basis for such processing, we generally obtain consent from the data subject.The processing of personal data, such as the name, address, e-mail address, or telephone number of a data subject shall always be in line with the General Data Protection Regulation (GDPR), and in accordance with the country-specific data protection regulations applicable to the Staking Facilities GmbH. By means of this data protection declaration, our enterprise would like to inform the general public of the nature, scope, and purpose of the personal data we collect, use and process. Furthermore, data subjects are informed, by means of this data protection declaration, of the rights to which they are entitled. As the controller, the Staking Facilities GmbH has implemented numerous technical and organizational measures to ensure the most complete protection of personal data processed through this website. However, Internet-based data transmissions may in principle have security gaps, so absolute protection may not be guaranteed. For this reason, every data subject is free to transfer personal data to us via alternative means, e.g. by telephone.
+								The temporary storage of this personal data is necessary to deliver the website to you. The legal basis for this processing of your personal data is Art. 6 (1) b) GDPR.
+							</Paragraph>
+							<h3><strong>1.2 Log data</strong></h3>
+							<Paragraph>
+								Further storage of this data takes place in log files in order to ensure the functionality of our website and, if necessary, to check and enforce our rights or property, our terms of use and the rights of third parties. The log files are stored as long as necessary for the respective purpose, usually no longer than 14 days. The legal basis is Art. 6 (1) f) GDPR.
+							</Paragraph>
+							<h3><strong>1.3. Other purposes for which we process your personal data</strong></h3>
+							<Paragraph>
+								<List>
+									<li>to fulfill contracts and in the context of existing or new business relationships. We will retain this personal data at least for the duration of our business relationship. The legal basis is Art. 6 (1) b) GDPR</li>
+									<li>to meet our legitimate interests (Art. 6 (1) f) GDPR), including the following:</li>
+									<List>
+										<li>Enter into corporate transactions (e.g., restructuring, asset deals, mergers)</li>
+										<li>Protect our rights or property, enforce our terms of use and legal notices, and provide for the establishment, exercise, and defense of legal claim</li>
+									</List>
+									<li>to fulfill our legal obligations, court orders or other binding decisions. The legal basis is Art. 6 (1) c) GDPR.</li>
+									<li>with your consent (Art. 6 (1) a) GDPR) for other purposes, such as subscription to a newsletter; in this case, you have the option to revoke your consent at any time.</li>
+								</List>
+							</Paragraph>
+							<h2><strong>2. Recipients of personal data</strong></h2>
+							<Paragraph>
+								In addition to the recipients of personal mentioned above, we also use other companies to process personal data on our behalf, for example for hosting of our website or sending newsletters. Such processors only process the data on our behalf (Art. 28 GDPR).
 							</Paragraph>
 							<Paragraph>
-								1. Definitions
+								Furthermore, we transmit personal data to third parties if this is required by law, necessary for the exercise and defense of legal claims or if third parties provide certain services for us (e.g., lawyers, tax consultants).
 							</Paragraph>
 							<Paragraph>
-								The data protection declaration of the Staking Facilities GmbH is based on the terms used by the European legislator for the adoption of the General Data Protection Regulation (GDPR). Our data protection declaration should be legible and understandable for the general public, as well as our customers and business partners. To ensure this, we would like to first explain the terminology used in this data protection declaration, we use, inter alia, the following terms:
+								Insofar as we transfer personal data to third countries outside the EU, we ensure an appropriate level of data protection through contractual measures where the level of protection has not been found adequate by the EU commission. You have the right to receive copies of these contracts.
+							</Paragraph>
+							<h2><strong>3. Regular deletion of personal data</strong></h2>
+							<Paragraph>
+								Unless a specific duration of data storage is specified in this Privacy Policy, we will only process your data as long as this is necessary for the respective purposes or as long as there are legal retention obligations. After the respective processing purpose ceases to apply and retention obligations end, your data will be routinely deleted.
+							</Paragraph>
+							<h2><strong>4. Your rights</strong></h2>
+							<Paragraph>
+								You can request access to the personal data stored by us. If you have provided personal data on the basis of a contract or consent, you have the right to receive this personal data in a standard and machine-readable format.
 							</Paragraph>
 							<Paragraph>
-								a) Personal data
+								You can also request the deletion, correction or restriction of the processing of your personal data subject to the conditions and limitations set out in the GDPR.
 							</Paragraph>
 							<Paragraph>
-								Personal data means any information relating to an identified or identifiable natural person ("data subject"). An identifiable natural person is one who can be identified, directly or indirectly, in particular by reference to an identifier such as a name, an identification number, location data, an online identifier or to one or more factors specific to the physical, physiological, genetic, mental, economic, cultural or social identity of that natural person.
+								You can revoke your consent at any time without affecting the lawfulness of processing based on your consent before its withdrawal.
 							</Paragraph>
 							<Paragraph>
-								b) Data subject
+								<strong>You can object to the processing of data processed on the basis of our legitimate interests at any time subject to the conditions and limitations set out in the GDPR.</strong>
+							</Paragraph>
+							<h2><strong>5. Contact; complaint to supervisory authority</strong></h2>
+							<Paragraph>
+								Please address your questions or concerns regarding the processing of your personal data to:
 							</Paragraph>
 							<Paragraph>
-								Data subject is any identified or identifiable natural person, whose personal data is processed by the controller responsible for the processing.
+								Staking Facilities Services GmbH & Co. KG<br/>
+								Rosenheimer Str. 18<br/>
+								85653 Aying<br/>
+								Germany
 							</Paragraph>
 							<Paragraph>
-								c) Processing
-							</Paragraph>
-							<Paragraph>
-								Processing is any operation or set of operations which is performed on personal data or on sets of personal data, whether or not by automated means, such as collection, recording, organisation, structuring, storage, adaptation or alteration, retrieval, consultation, use, disclosure by transmission, dissemination or otherwise making available, alignment or combination, restriction, erasure or destruction.
-							</Paragraph>
-							<Paragraph>
-								d) Restriction of processing
-							</Paragraph>
-							<Paragraph>
-								Restriction of processing is the marking of stored personal data with the aim of limiting their processing in the future.
-							</Paragraph>
-							<Paragraph>
-								e) Profiling
-							</Paragraph>
-							<Paragraph>
-								Profiling means any form of automated processing of personal data consisting of the use of personal data to evaluate certain personal aspects relating to a natural person, in particular to analyse or predict aspects concerning that natural person's performance at work, economic situation, health, personal preferences, interests, reliability, behaviour, location or movements.
-							</Paragraph>
-							<Paragraph>
-								f) Pseudonymisation
-							</Paragraph>
-							<Paragraph>
-								Pseudonymisation is the processing of personal data in such a manner that the personal data can no longer be attributed to a specific data subject without the use of additional information, provided that such additional information is kept separately and is subject to technical and organisational measures to ensure that the personal data are not attributed to an identified or identifiable natural person.
-							</Paragraph>
-							<Paragraph>
-								g) Controller or controller responsible for the processing
-							</Paragraph>
-							<Paragraph>
-								Controller or controller responsible for the processing is the natural or legal person, public authority, agency or other body which, alone or jointly with others, determines the purposes and means of the processing of personal data; where the purposes and means of such processing are determined by Union or Member State law, the controller or the specific criteria for its nomination may be provided for by Union or Member State law.
-							</Paragraph>
-							<Paragraph>
-								h) Processor
-							</Paragraph>
-							<Paragraph>
-								Processor is a natural or legal person, public authority, agency or other body which processes personal data on behalf of the controller.
+								<a href="mailto:info@stakingfacilities.com" title="Contact Us">info@stakingfacilities.com</a>
 							</Paragraph>
 						</Content>
 					</Container>
@@ -94,8 +102,21 @@ const Container = styled.div`
 const Content = styled.div`
 	overflow-x: auto;
 	padding-bottom: 2rem;
+	
+	h2,h3 {
+		padding-top: 3rem;
+	}
 `
 
 const Paragraph = styled.p`
 	padding: 1rem 0;
+`
+
+const List = styled.ul`
+	list-style: disc;
+	padding: 1rem 0 1rem 2rem;
+	
+	ul {
+		list-style-type: '- ';
+	}
 `
